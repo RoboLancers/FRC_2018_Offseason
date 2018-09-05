c329caaf3b7c41f7276a00bdb35a9162b0ff8141
Brian Chen <brianchen0810@hotmail.com>
2018-09-05 19:55:35 -0400

    Add utility methods

---
+++ src/main/java/frc/team321/robot/utilities/RobotUtil.java
@@ -0,0 +1,67 @@
+package frc.team321.robot.utilities;
+
+public class RobotUtil {
+    /**
+     * Limits value to a specified minimum and maximum
+     *
+     * @param value The value to check
+     * @param min   The lower bound
+     * @param max   The upper bound
+     * @return Returns a number that is between min and max inclusive
+     */
+    public static double range(double value, double min, double max) {
+        return Math.min(max, Math.max(min, value));
+    }
+
+    /**
+     * Helper method for case where min and max are same number but one has a
+     * negative sign
+     *
+     * @param value The value to limit
+     * @param max   The maximum value the number can take
+     * @return Returns a number that is between -max and -max inclusive
+     * @see RobotUtil#range(double, double, double)
+     */
+    public static double range(double value, double max) {
+        return range(value, -max, max);
+    }
+
+    /**
+     * Floors a number to a specific number of decimal places
+     *
+     * @param num    The number to floor
+     * @param places How many decimal places to use
+     * @return Returns the number floored
+     */
+    public static double floor(double num, int places) {
+        double multiplier = 1;
+
+        for (int x = 0; x < places; x++) {
+            multiplier *= 10;
+        }
+
+        num *= multiplier;
+
+        return (int) num / multiplier;
+    }
+
+    /**
+     * Keep the sign when squaring
+     *
+     * @param num The number to square
+     * @return The number squared including signs
+     */
+    public static double squareKeepSign(double num) {
+        return num < 0 ? -(num * num) : (num * num);
+    }
+
+    /**
+     * Keep the sign when square rooting
+     *
+     * @param num The number to square root
+     * @return The number square rooted including signs
+     */
+    public static double sqrtKeepSign(double num) {
+        return num < 0 ? -(Math.sqrt(-num)) : Math.sqrt(num);
+    }
+}
