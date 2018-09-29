e693b4f80747c77d952c90acc2f316394064fa41
Brian Chen <brianchen0810@hotmail.com>
2018-09-28 22:11:05 -0400

    Add P value for PID

--- src/main/java/frc/team321/robot/Constants.java
+++ src/main/java/frc/team321/robot/Constants.java
@@ -2,13 +2,13 @@ package frc.team321.robot;
 
 public class Constants {
     // TalonSRXs
-    public static final int LEFT_MASTER_MOTOR = 10;
-    public static final int LEFT_SLAVE_1 = 5;
+    public static final int LEFT_MASTER_MOTOR = 5;
+    public static final int LEFT_SLAVE_1 = 10;
     public static final int LEFT_SLAVE_2 = 12;
 
-    public static final int RIGHT_MASTER_MOTOR = 9;
+    public static final int RIGHT_MASTER_MOTOR = 8;
     public static final int RIGHT_SLAVE_1 = 4;
-    public static final int RIGHT_SLAVE_2 = 8;
+    public static final int RIGHT_SLAVE_2 = 9;
 
     public static final int LINEAR_MASTER = 11;
     public static final int LINEAR_SLAVE = 2;
@@ -41,15 +41,17 @@ public class Constants {
     public static final int DRIVETRAIN_ENCODER_TICKS_PER_REVOLUTION = 1024;
     public static final double DRIVETRAIN_MAX_RPM = 571.3;
 
-    public static final double DRIVETRAIN_MOTION_PROFILE_P = 0.6;
+    public static final double DRIVETRAIN_MOTION_PROFILE_P = 0.025;
     public static final double DRIVETRAIN_MOTION_PROFILE_I = 0;
     public static final double DRIVETRAIN_MOTION_PROFILE_D = 0;
 
     public static final double DRIVETRAIN_WHEEL_DIAMETER_INCHES = 6;
-    public static final double DRIVETRAIN_MAX_VELOCITY = 8; //6 feet per second
+    public static final double DRIVETRAIN_MAX_VELOCITY = 6; //6 feet per second
 
     public static final double DRIVETRAIN_KV = 1/DRIVETRAIN_MAX_VELOCITY;
     public static final double DRIVETRAIN_KA = 0.0;
 
-    public static final double DRIVETRAIN_ROTATE_P = 0.005;
+    public static final double DRIVETRAIN_ROTATE_P = 0.01;
+
+    public static final double DRIVETRAIN_PID_P = 0.025;
 }
