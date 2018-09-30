1c5a8210079fac73f2cfd163332870ac12268cbe
Brian Chen <brianchen0810@hotmail.com>
2018-09-29 20:25:14 -0400

    Change to use TimedRobot

--- src/main/java/frc/team321/robot/Robot.java
+++ src/main/java/frc/team321/robot/Robot.java
@@ -1,22 +1,22 @@
 package frc.team321.robot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.team321.robot.subsystems.drivetrain.Drivetrain;
 import frc.team321.robot.subsystems.manipulator.Manipulator;
 import frc.team321.robot.subsystems.misc.GripPipeline;
 import frc.team321.robot.subsystems.misc.Pneumatic;
-import frc.team321.robot.subsystems.misc.Sensors;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
-public class Robot extends IterativeRobot {
+public class Robot extends TimedRobot {
 
     public static Drivetrain drivetrain;
     public static Manipulator manipulator;
@@ -66,6 +66,7 @@ public class Robot extends IterativeRobot {
         drivetrain.enableRamping(false);
         drivetrain.getGearShifter().setLowGear();
         drivetrain.resetEncoders();
+        drivetrain.setMode(NeutralMode.Brake);
 
         autonomousCommand = oi.getAutoCommand(oi.getAutoMode());
 
