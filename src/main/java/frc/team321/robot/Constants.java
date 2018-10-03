4ea176a0215f28d9bc09e6baa43498896317db8c
Brian Chen <brianchen0810@gmail.com>
2018-10-03 15:35:49 -0400

    Tune constants for motion profiling

--- src/main/java/frc/team321/robot/Constants.java
+++ src/main/java/frc/team321/robot/Constants.java
@@ -39,21 +39,21 @@ public class Constants {
     public static final int DRIVETRAIN_PRIMARY_PID_LOOP = 0;
 
     public static final int DRIVETRAIN_ENCODER_TICKS_PER_REVOLUTION = 1024;
-    public static final double DRIVETRAIN_MAX_RPM = 571.3;
+    public static final double DRIVETRAIN_MAX_RPM = 527.34;
 
-    public static final double DRIVETRAIN_MOTION_PROFILE_P = 0.025;
+    public static final double DRIVETRAIN_MOTION_PROFILE_P = 0.01;
     public static final double DRIVETRAIN_MOTION_PROFILE_I = 0;
     public static final double DRIVETRAIN_MOTION_PROFILE_D = 0;
 
     public static final double DRIVETRAIN_WHEEL_DIAMETER_INCHES = 6;
-    public static final double DRIVETRAIN_MAX_VELOCITY = 6; //6 feet per second
+    public static final double DRIVETRAIN_MAX_VELOCITY = 12; //12 feet per second
 
     public static final double DRIVETRAIN_KV = 1/DRIVETRAIN_MAX_VELOCITY;
     public static final double DRIVETRAIN_KA = 0.0;
 
     public static final double DRIVETRAIN_ROTATE_P = 0.01;
 
-    public static final double DRIVETRAIN_PID_P = 0.025;
+    public static final double DRIVETRAIN_PID_P = 0.01;
 
     public static final int DRIVETRAIN_PEAK_CURRENT_LIMIT = 80;
     public static final int DRIVETRAIN_SUSTAINED_CURRENT_LIMIT = 36;
