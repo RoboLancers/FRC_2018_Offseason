7951ffd888f31027f587dd8f9ec1165ee9c4a71d
Brian Chen <brianchen0810@hotmail.com>
2018-10-06 18:42:46 -0400

    Add Odometry for Ramsete

--- src/main/java/frc/team321/robot/utilities/Odometry.java
+++ src/main/java/frc/team321/robot/utilities/Odometry.java
@@ -1,12 +1,21 @@
 package frc.team321.robot.utilities;
 
-public class Odometry {
-    private double x, y, theta;
+import frc.team321.robot.Robot;
 
-    public Odometry(double x, double y, double theta){
-        this.x = x;
-        this.y = y;
-        this.theta = theta;
+public enum Odometry {
+    INSTANCE;
+
+    private volatile double x, y, theta;
+    private volatile double currentEncoderPosition, lastPosition, deltaPosition;
+
+    Odometry(){
+        this.x = 0;
+        this.y = 0;
+        this.theta = 0;
+
+        this.currentEncoderPosition = 0;
+        this.lastPosition = (Robot.drivetrain.getLeft().getEncoderCount() + Robot.drivetrain.getRight().getEncoderCount()) / 2.0;
+        this.deltaPosition = 0;
     }
 
     public double getX() {
@@ -21,16 +30,48 @@ public class Odometry {
         return theta % (2 * Math.PI);
     }
 
-    public void addX(double x){
+    public synchronized void setX(double x){
+        this.x = x;
+    }
+
+    public synchronized void setY(double y){
+        this.y = y;
+    }
+
+    public synchronized void addX(double x){
         this.x += x;
     }
 
-    public void addY(double y){
+    public synchronized void addY(double y){
         this.y += y;
     }
 
-    public void addTheta(double theta){
-        this.theta += theta;
+    public synchronized void setTheta(double theta){
+        this.theta = theta;
+    }
+
+    public double getCurrentEncoderPosition() {
+        return currentEncoderPosition;
+    }
+
+    public double getLastPosition() {
+        return lastPosition;
+    }
+
+    public double getDeltaPosition() {
+        return deltaPosition;
+    }
+
+    public synchronized void setCurrentEncoderPosition(double currentEncoderPosition) {
+        this.currentEncoderPosition = currentEncoderPosition;
+    }
+
+    public synchronized void setLastPosition(double lastPosition) {
+        this.lastPosition = lastPosition;
+    }
+
+    public synchronized void setDeltaPosition(double deltaPosition) {
+        this.deltaPosition = deltaPosition;
     }
 
     public String toString(){
