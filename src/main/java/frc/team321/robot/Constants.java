6ff37276d7d768af40404554a677d91fb2cc2668
Brian Chen <brianchen0810@hotmail.com>
2018-10-13 21:57:22 -0400

    Move ports into RobotMap

--- src/main/java/frc/team321/robot/Constants.java
+++ src/main/java/frc/team321/robot/Constants.java
@@ -1,38 +1,6 @@
 package frc.team321.robot;
 
 public class Constants {
-    // TalonSRXs
-    public static final int LEFT_MASTER_MOTOR = 5;
-    public static final int LEFT_SLAVE_1 = 10;
-    public static final int LEFT_SLAVE_2 = 12;
-
-    public static final int RIGHT_MASTER_MOTOR = 8;
-    public static final int RIGHT_SLAVE_1 = 4;
-    public static final int RIGHT_SLAVE_2 = 9;
-
-    public static final int LINEAR_MASTER = 11;
-    public static final int LINEAR_SLAVE = 2;
-
-    public static final int INTAKE_LEFT = 6;
-    public static final int INTAKE_RIGHT = 7;
-
-    // Compressor
-    public static final int COMPRESSOR = 0;
-
-    // Pneumatic
-    public static final int GEARSHIFTER_FORWARD = 0;
-    public static final int GEARSHIFTER_REVERSE = 1;
-
-    public static final int INTAKE_PIVOT_FORWARD = 4;
-    public static final int INTAKE_PIVOT_REVERSE = 5;
-
-    // Sensors
-    public static final int ULTRASONIC_TRIG = 0;
-    public static final int ULTRASONIC_ECHO = 1;
-
-    public static final int TOP_TOUCH_SENSOR = 8;
-    public static final int BOTTOM_TOUCH_SENSOR = 9;
-
     // Drivetrain Constants
     public static final int DRIVETRAIN_TIMEOUT_MS = 10;
     public static final int DRIVETRAIN_PID_SLOT_INDEX = 0;
